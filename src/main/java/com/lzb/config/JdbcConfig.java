2e524ca5e652f1e34cdba73053f5b824a5ad6efc
lzb <1255863672@qq.com>
2019-11-05 09:48:04 +0800

    修改pom.xml

---
+++ src/main/java/com/lzb/config/JdbcConfig.java
@@ -0,0 +1,7 @@
+package com.lzb.config;
+
+/**
+ * Created by luzhibin on 2019/11/4 20:33
+ */
+public class JdbcConfig {
+}
---
+++ src/main/java/com/lzb/config/JdbcProperties.java
@@ -0,0 +1,7 @@
+package com.lzb.config;
+
+/**
+ * Created by luzhibin on 2019/11/5 0:18
+ */
+public class JdbcProperties {
+}
